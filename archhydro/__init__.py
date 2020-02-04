dc3add6a72e2dc1e83f499bb36a348594c726c42
kmarkert <kel.markert@gmail.com>
2020-02-04 13:36:49 -0600

    updated package imports and added package version

--- archhydro/__init__.py
+++ archhydro/__init__.py
@@ -1,4 +1,8 @@
-from .core import *
-from .forcings import Forcing
-from . import authorization
-from . import pet
+from archhydro.core import *
+from archhydro.forcings import Forcing
+from archhydro.parameters import Parameter
+from archhydro import authorization
+from archhydro import pet
+from archhydro import utils
+
+__version__ = "0.0.1"
