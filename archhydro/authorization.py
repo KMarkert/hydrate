bed14b6dedb2052892790ed4a9515346d81b1212
kmarkert <kel.markert@gmail.com>
2020-02-04 13:34:12 -0600

    updated authorization module and functions to get requests session

--- archhydro/authorization.py
+++ archhydro/authorization.py
@@ -1,15 +1,30 @@
 from oauth2client.service_account import ServiceAccountCredentials
 import httplib2
 
+from google.auth.transport.requests import AuthorizedSession
+from google.oauth2 import service_account
+
+
+def getSession(key):
+    credentials = service_account.Credentials.from_service_account_file(key)
+    scoped_credentials = credentials.with_scopes(
+        ['https://www.googleapis.com/auth/cloud-platform'])
+
+    return AuthorizedSession(scoped_credentials)
+
 # authorize with json file
+
+
 def authorize(SERVICE_ACCOUNT, SERVICE_KEY):
     SCOPES = ['https://www.googleapis.com/auth/earthengine.readonly']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_KEY,scopes=SCOPES)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        SERVICE_KEY, scopes=SCOPES)
 
     http = creds.authorize(httplib2.Http())
 
     return http
 
-def authorize_using_pk12(SERVICE_ACCOUNT,SERVICE_KEY):
+
+def authorize_using_pk12(SERVICE_ACCOUNT, SERVICE_KEY):
 
     return
