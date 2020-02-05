8b71b0e633c20b67b0b0e6e35bd8fe7f20bc2d93
kmarkert <kel.markert@gmail.com>
2020-02-05 16:01:48 -0600

    renamed package

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='archhydro',
+setup(name='hydrate',
       version='0.0.1',
-      description='ArchHydro is a Python package to setup and run hydrologic models using data from Earth Engine',
+      description='Hydrate is a Python package to setup and run hydrologic models using data from Earth Engine',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url='http://github.com/kmarkert/archhydro',
+      url='http://github.com/kmarkert/hydrate',
       packages=setuptools.find_packages(),
       author='Kel Markert',
       author_email='kel.markert@gmail.com',
